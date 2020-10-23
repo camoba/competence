3bd237787d7024b1794d883a7d946104c97e9f3b
mbannert <bunny@hey.com>
2020-10-23 22:16:05 +0200

    Rmd report example

--- playground.R
+++ playground.R
@@ -30,13 +30,24 @@ res_num <- res[, .SD,
                .SDcols = sapply(res, is.numeric)]
 res_num[,id := 1:nrow(res_num)]
 res_m <- melt(res_num,id.vars = "id")
+avg <- res_m[, list(avg = mean(value)), variable]
+
+gg_avg <- ggplot(data = avg)
+gg_avg + geom_bar(aes(x = variable,
+                      y = avg),
+                  stat = "identity") +
+  coord_flip() +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.spacing = unit(4, "lines"),
+        text = element_text(size=15))
 
 
-
-gg <- ggplot(data = res_m)
-gg +
+gg_read <- ggplot(data = res_m[variable %in% c("prepare",
+                                          "reading","foreign"),])
+gg_read +
   geom_bar(aes(x = as.factor(value), fill = variable)) +
-  facet_wrap("variable", nrow = 2) +
+  facet_wrap("variable", nrow = 1) +
   theme_minimal() +
   theme(panel.grid.major.x = element_blank(),
         panel.spacing = unit(4, "lines"),
@@ -46,9 +57,41 @@ gg +
   scale_fill_viridis_d()
 
 
+gg_soft <- ggplot(data = res_m[variable %in% c("team",
+                                               "critique","self"),])
+gg_soft +
+  geom_bar(aes(x = as.factor(value), fill = variable)) +
+  facet_wrap("variable", nrow = 1) +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.spacing = unit(4, "lines"),
+        text = element_text(size=15)) +
+  scale_x_discrete(name ="Skill",
+                   limits=factor(1:5)) +
+  scale_fill_viridis_d()
+
+
+gg_research <- ggplot(data = res_m[variable %in% c("research"),])
+gg_research +
+  geom_bar(aes(x = as.factor(value), fill = variable)) +
+  facet_wrap("variable", nrow = 1) +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.spacing = unit(4, "lines"),
+        text = element_text(size=15)) +
+  scale_x_discrete(name ="Skill",
+                   limits=factor(1:5)) +
+  scale_fill_viridis_d()
+
+
+
+
 unique(res$missing[res$missing != ""])
 
+nchar("Texte kritisch zu hinterfragen und nicht einfach nur des Informationsgehalts wegens zu lesen.")
 
-
-
+out_text <- unique(res$missing[res$missing != ""])
+out_stripped <- substr(out_text,1,95)
+out_stripped[nchar(out_text) > 95] <- paste0(out_stripped[nchar(out_text) > 95],"[...]")
+out_stripped
 
